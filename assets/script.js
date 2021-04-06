f57b792d2c3e06e8be1d809d49986a86eb32c5da
mooreFL <moorepr09@gmail.com>
2021-04-06 17:23:01 -0400

    fixed api key issue and added comments

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,4 @@
+var apiKey = "a5add3ea86991b37abdb4b61b61f9a2a";
 var todayElement = document.getElementById("today");
 var forecastEl = document.getElementById("forecast");
 var searchButton = document.getElementById("search-button");
